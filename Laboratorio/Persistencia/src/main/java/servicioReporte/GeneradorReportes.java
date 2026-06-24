f9ab17cf65d63b5b85c31032e4b5930826ba95db
michelle8q <cincastle584@gmail.com>
2026-06-24 08:39:42 -0700

    dependencia de jasper

---
+++ Laboratorio/Persistencia/src/main/java/servicioReporte/GeneradorReportes.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servicioReporte;
+
+import java.io.InputStream;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
+
+/**
+ *
+ * @author cinca
+ */
+public class GeneradorReportes {
+   // public void generarInformeProductos(List<CatalogoProductosDTO> parametros) {
+     //   try {
+          //  JRBeanCollectionDataSource datos = new JRBeanCollectionDataSource(productos);
+          //  InputStream reporte = getClass().getResourceAsStream("/reportes/ReporteProductos.jasper");
+          //  JasperPrint reporteLleno = JasperFillManager.fillReport(reporte, null, datos);
+          //  JasperViewer.viewReport(reporteLleno,false);
+
+       // } catch(Exception e){
+         //   e.printStackTrace();
+      //  }
+        
+   // }
+}
