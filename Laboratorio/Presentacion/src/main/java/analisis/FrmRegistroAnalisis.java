160276d250f9e6fb2014ad7fb461be03102d9175
michelle8q <cincastle584@gmail.com>
2026-06-23 22:47:22 -0700

    Renombre de frm

--- Laboratorio/Presentacion/src/main/java/analisis/FrmRegistroAnalisis.java
+++ Laboratorio/Presentacion/src/main/java/analisis/FrmRegistroAnalisis.java
@@ -8,12 +8,12 @@ package analisis;
  *
  * @author cinca
  */
-public class registroAnalisis extends javax.swing.JFrame {
+public class FrmRegistroAnalisis extends javax.swing.JFrame {
 
     /**
      * Creates new form registroAnalisis
      */
-    public registroAnalisis() {
+    public FrmRegistroAnalisis() {
         initComponents();
     }
 
@@ -168,20 +168,21 @@ public class registroAnalisis extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(registroAnalisis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmRegistroAnalisis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(registroAnalisis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmRegistroAnalisis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(registroAnalisis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmRegistroAnalisis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(registroAnalisis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmRegistroAnalisis.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new registroAnalisis().setVisible(true);
+                new FrmRegistroAnalisis().setVisible(true);
             }
         });
     }
--- Laboratorio/Presentacion/src/main/java/analisis/FrmRegistroParametros.java
+++ Laboratorio/Presentacion/src/main/java/analisis/FrmRegistroParametros.java
@@ -8,12 +8,12 @@ package analisis;
  *
  * @author cinca
  */
-public class registroParametros extends javax.swing.JFrame {
+public class FrmRegistroParametros extends javax.swing.JFrame {
 
     /**
      * Creates new form registroAnalisis
      */
-    public registroParametros() {
+    public FrmRegistroParametros() {
         initComponents();
     }
 
@@ -293,21 +293,23 @@ public class registroParametros extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(registroParametros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmRegistroParametros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(registroParametros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmRegistroParametros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(registroParametros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmRegistroParametros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(registroParametros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmRegistroParametros.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new registroParametros().setVisible(true);
+                new FrmRegistroParametros().setVisible(true);
             }
         });
     }
